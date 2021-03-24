1db7fd826c9630279f9b6fdcb59d6f39f5eb4bad
Kevin Mulcrone <kaybesee@gmail.com>
2021-03-23 23:15:37 -0600

    adjust image, fix broken link

--- components/Image.js
+++ components/Image.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const ImageComponent = ({ src, alt }) => (
-  <div className="rounded-md shadow-2xl overflow-hidden m-8 relative">
+  <div className="rounded-md shadow-2xl overflow-hidden my-8 mx-2 md:mx-8 relative">
     <Image
       src={src}
       alt={alt}
